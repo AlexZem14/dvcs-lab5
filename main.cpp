d0154d1673261a276de50d7c99a6e6d0032cd6d4
GolovinovaK <ksenyaaurinko@gmail.com>
2019-04-17 12:10:12 +0300

    master commit 1

---
+++ main.cpp
@@ -0,0 +1,87 @@
+/*! \file main.cpp
+ * Главный файл программы
+ */
+
+#include "synhra.h"
+
+/*!
+ * \brief Главная функция программы
+ * \param [in] argc Количество переданных аргументов командной строки
+ * \param [in] argv Массив аргументов командной строки
+ * \return 0-при успешном выполнении, 1-в противном случае
+ * 1 commit master
+ * Аргументы командной строки:
+ * argv[0] - аргумент содержащий имя запускаемой программы;
+ * argv[1] – аргумент содержащий имя первого исходного файла;
+ * argv[2] – аргумент содержащий имя второго исходного файла;
+ * argv[3] – аргумент содержащий имя выходного файла
+ */
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    _setmode(_fileno(stderr), _O_U8TEXT); // Режим вывода в stderr в кодировке UTF-8
+
+    error error;
+    if(!checkArguments(argc, argv, error))
+    {
+        QString errorDescription = "Ошибка параметров программы: Нет ";
+        if(error.type == errorType::noFileIn)
+        {
+            errorDescription += "входного ";
+        }
+        else if(error.type == errorType::noNameOut)
+        {
+            errorDescription += "выходного ";
+        }
+        errorDescription += "файла";
+        qError(errorDescription);
+        return 1;
+    }
+
+    QStringList matchList;
+    if(!readMatch(argv[1], matchList, error))
+    {
+        QString errorDescription = "Ошибка параметров программы: Файл с соответствиями ";
+        if(error.type == errorType::noExistFile)
+        {
+            errorDescription += "не существует";
+        }
+        else if(error.type == errorType::noOpenFile)
+        {
+            errorDescription += "не получается открыть";
+        }
+        qError(errorDescription);
+        return 1;
+    }
+
+    QString stringChain;
+    if(!readChain(argv[2], stringChain, error))
+    {
+        QString errorDescription = "Ошибка параметров программы: Файл c последовательностью действий ";
+        if(error.type == errorType::noExistFile)
+        {
+            errorDescription += "не существует";
+        }
+        else if(error.type == errorType::noOpenFile)
+        {
+            errorDescription += "не получается открыть";
+        }
+        qError(errorDescription);
+        return 1;
+    }
+
+    QMap<int, QString> match;
+    if(!putToMap(matchList, match))
+        return 1;
+
+    QVector<int> chain;
+    if(!putToVector(stringChain, chain, match))
+        return 1;
+
+    QVector<QString> mappedModes;
+    matchModes(match, chain, mappedModes);
+
+    if(!writeData(mappedModes,argv[3]))
+        return 1;
+    return 0;
+}
